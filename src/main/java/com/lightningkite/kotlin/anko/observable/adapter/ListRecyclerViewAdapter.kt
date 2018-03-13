7d1afca5f596e329a7ecd5b311ac4598700ca2d5
iverc <ivan@pasturemap.com>
2018-03-13 17:21:54 +0200

    Update recycler view adapters

--- src/main/java/com/lightningkite/kotlin/anko/observable/adapter/ListRecyclerViewAdapter.kt
+++ src/main/java/com/lightningkite/kotlin/anko/observable/adapter/ListRecyclerViewAdapter.kt
@@ -48,7 +48,7 @@ open class ListRecyclerViewAdapter<T>(
 
     var default: T? = null
     var shouldSetDefault = true
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<T>? {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<T> {
         if (shouldSetDefault) {
             default = list.first()
             shouldSetDefault = false
